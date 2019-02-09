6e170c4efd8ba54fb66049dcf356fcd76c50051e
Kameron Meyer <normakster@gmail.com>
2019-02-08 16:35:29 -0800

    Fixed refactoring

--- rest/app/controllers/utilController.js
+++ rest/app/controllers/utilController.js
@@ -1,6 +1,7 @@
 const { MongoClient, ObjectId } = require('mongodb');
 const debug = require('debug')('app:utilController');
-var config = require('../config.js');
+// var config = require('../config.js');
+var config = require('../../config/config.js');
 var mongoose = require('mongoose');
 
 // const url = 'mongodb://'+config.db.USER+':'+config.db.PWD+'@'+config.db.HOST+':'+config.db.PORT+'/?authMechanism=SCRAM-SHA-1&authSource='+config.db.database;
